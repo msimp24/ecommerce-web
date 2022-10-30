01ee2732c893e0669ea4446993efec464db1d4cb
msimp24 <74673535+msimp24@users.noreply.github.com>
2022-10-30 16:32:35 +0200

    commit

--- public/app.js
+++ public/app.js
@@ -1,13 +1,8 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-});
-
-    const homeBtn = document.querySelector('#home-button');
-    const fictionBtn = document.querySelector('#fiction-button');
-    const test = document.getElementById('#test');
-
-
-test.addEventListener('click', ()=>{
-    alert("hello world")
+const homeBtn = document.querySelector('#home-button');
+const fictionBtn = document.querySelector('#fiction-button')
+const test = document.getElementById('#test');
     
-})
+
+
+
+
