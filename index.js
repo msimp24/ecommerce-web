3141ae7c4c9c25dae6f32d47975f30e85dba5051
msimp24 <74673535+msimp24@users.noreply.github.com>
2022-10-30 14:01:49 +0200

    additional html created

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require('express')
 const app = express();
+const path = require('path');
+const router = express.Router();
 
-app.listen(3000, ()=>{
-    console.log('listening at port 3000');
-})
 
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
+
+router.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+    console.log('Hello world');
+})
+
+router.get('/home', function(req, res){
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+})
+
+router.get('/fiction', function(req, res){
+    res.sendFile(path.join(__dirname+'/public/fiction.html'));
+})
+
+router.get('/non-fiction', function(req, res){
+    res.sendFile(path.join(__dirname+'/public/non-fiction.html'));
+})
+
+router.get('/biographies', function(req, res){
+    res.sendFile(path.join(__dirname+'/public/biographies.html'));
+})
+router.get('/kids', function(req, res){
+    res.sendFile(path.join(__dirname+'/public/kids.html'));
+})
+
+
+app.use('/', router);
+app.listen(3000, ()=>{
+    console.log('listening at port 3000');
+})
---
+++ public/app.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+});
+
+    const homeBtn = document.querySelector('#home-button');
+    const fictionBtn = document.querySelector('#fiction-button');
+    const test = document.getElementById('#test');
+
+
+test.addEventListener('click', ()=>{
+    alert("hello world")
+    
+})
